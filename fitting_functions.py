0f94d9615f98867364a0fb5b169b8914dc35b48b
Zachary Reedy <zacharyreedy@Zacharys-MacBook-Air-2.local>
2025-11-14 12:30:50 -0500

    Established functions for line fitting

---
+++ fitting_functions.py
@@ -0,0 +1,12 @@
+def linear(m,x,b):
+    return m*x+b
+
+def slope_units(x_units,y_units):
+    x_stripped = x_units.rstrip("s")
+    y_stripped = y_units.rstrip("s")
+    return str(x_stripped) + "/" + str(y_stripped)
+
+def print_equation(m,b,x_units,y_units):
+    m_units = slope_units(x_units, y_units)
+    print("The equation of the line is: y = " + str(m) + " " + m_units + " x + " + str(b) + " in")
+    
